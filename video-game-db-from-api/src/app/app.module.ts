b59b3bb33a54b09d7bdc4ad4e6e8e32da8ab48e9
Clinton F <legacy_7005@yahoo.com>
2021-07-14 14:33:49 -0400

    first commit

---
+++ video-game-db-from-api/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { GaugeModule } from 'angular-gauge';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+
+
+
+import { AppComponent } from './app.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { HomeComponent } from './components/home/home.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchBarComponent,
+    HomeComponent
+  ],
+  imports: [
+    FormsModule,
+    GaugeModule.forRoot(),
+    MatTabsModule,
+    BrowserModule,
+    MatIconModule,
+    MatSelectModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
